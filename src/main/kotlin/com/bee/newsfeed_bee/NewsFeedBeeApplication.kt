2013a6f2f965bd65fa4780167bf6b737da330098
codingjang <jjangga813@naver.com>
2024-01-08 15:07:27 +0900

    Git Init New Project Started

---
+++ src/main/kotlin/com/bee/newsfeed_bee/NewsFeedBeeApplication.kt
@@ -0,0 +1,11 @@
+package com.bee.newsfeed_bee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewsFeedBeeApplication
+
+fun main(args: Array<String>) {
+    runApplication<NewsFeedBeeApplication>(*args)
+}
---
+++ src/test/kotlin/com/bee/newsfeed_bee/NewsFeedBeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bee.newsfeed_bee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsFeedBeeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
