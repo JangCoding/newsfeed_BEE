f3557f2e00fbe948c7bbcea8a602657b3a7c1a3c
codingjang <jjangga813@naver.com>
2024-01-09 17:50:53 +0900

    ReplyRepository 메서드 추가 findByIdAndDeletedDateTimeIsNull

---
+++ src/main/kotlin/com/bee/newsfeed_bee/domain/reply/entity/Reply.kt
@@ -0,0 +1,52 @@
+package com.bee.newsfeed_bee.domain.reply.entity
+
+import com.bee.newsfeed_bee.domain.feed.entity.Feed
+import com.bee.newsfeed_bee.domain.reply.dto.ReplyResponse
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.*
+import org.springframework.data.annotation.Id
+import java.time.OffsetDateTime
+
+
+@Entity
+@Table(name = "feed")
+
+class Reply(
+    @Column(name="username")
+    var userName : String,
+
+    @JsonIgnore
+    @Column(name = "password")
+    var password : String,
+
+    @Column(name = "contents")
+    var contents : String,
+
+    @Column(name = "created_date_time")
+    var createdAt: OffsetDateTime = OffsetDateTime.now(),
+
+    @Column(name = "last_modified_time")
+    var updatedAt : OffsetDateTime ?= null,
+
+    @Column(name = "deleted_date_time")
+    var deletedAt : OffsetDateTime ?= null,
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    var feed: Feed
+){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id:Long?=null
+}
+
+fun Reply.toResponse(): ReplyResponse {
+    return ReplyResponse(
+        id= id!!,
+        userName = userName,
+        contents = contents,
+        createdAt =createdAt,
+        updatedAt=updatedAt,
+    )
+}
--- src/main/kotlin/com/bee/newsfeed_bee/domain/reply/repository/ReplyRepository.kt
+++ src/main/kotlin/com/bee/newsfeed_bee/domain/reply/repository/ReplyRepository.kt
@@ -1,8 +1,9 @@
 package com.bee.newsfeed_bee.domain.reply.repository
 
-import com.bee.newsfeed_bee.domain.reply.model.Reply
+import com.bee.newsfeed_bee.domain.reply.entity.Reply
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ReplyRepository : JpaRepository<Reply, Long>{
     fun findAllByFeedId(feedId:Long):List<Reply>
+    fun findByIdAndDeletedDateTimeIsNull(id:Long):Reply
 }
