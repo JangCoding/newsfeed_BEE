f9f0009ebcb0781afe006b4632304d670aff54ae
rugii913 <rugii913@gmail.com>
2024-01-09 11:32:52 +0900

    DB 스키마에 맞춰 deletedDateTime 타입을 nullable로 변경

--- src/main/kotlin/com/bee/newsfeed_bee/feed/entity/Feed.kt
+++ src/main/kotlin/com/bee/newsfeed_bee/feed/entity/Feed.kt
@@ -11,7 +11,7 @@ import java.time.LocalDateTime
 class Feed( // TODO: 수정 가능한 속성에 대해서는 더 논의할 필요가 있음
     val userId: Long, // TODO: User entity는 별도로 작업하고 있으므로 추후 User entity merge 시 변경할 것
     var deleted: Boolean,
-    var deletedDateTime: LocalDateTime,
+    var deletedDateTime: LocalDateTime?,
     var storeName: String, // store
     var category: String, // cuisineType? // enum // store
     var address: String, // store
