3a558ebe0576798f4b83e099dc910b9ba780600f
codingjang <jjangga813@naver.com>
2024-01-12 15:49:19 +0900

    예외 검증 처리 위치 회의 후 수정 - controller에서 데이터를 받아올 때 와 domain 에서 값을 처리할 때 둘 다 검증하는 것이 안정적일 것으로 판단하여 @Valid 어노테이션을 사용하는 것을 양립하기로 결정하였습니다.

--- src/main/kotlin/com/bee/newsfeed_bee/domain/reply/dto/ReplyDeleteRequest.kt
+++ src/main/kotlin/com/bee/newsfeed_bee/domain/reply/dto/ReplyDeleteRequest.kt
@@ -3,6 +3,6 @@ package com.bee.newsfeed_bee.domain.reply.dto
 import jakarta.validation.constraints.NotBlank
 
 data class ReplyDeleteRequest (
-    //@field:NotBlank(message = "password는 필수 입력 정보입니다.")
+    @field:NotBlank(message = "password는 필수 입력 정보입니다.")
     val password:String
 )
